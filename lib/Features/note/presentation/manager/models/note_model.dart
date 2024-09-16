345ca8e83de31d3888bc6a7b71748a45973a59af
Youssef <youssefabdallah2016@gmail.com>
2024-09-17 02:31:42 +0300

    Create NoteModelAdapter

--- lib/Features/note/presentation/manager/models/note_model.dart
+++ lib/Features/note/presentation/manager/models/note_model.dart
@@ -1,13 +1,18 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
   final String title;
   final String subTitle;
   final String date;
   final int color;
 
   NoteModel({
-    required this.title,
-    required this.subTitle,
-    required this.date,
-    required this.color,
+    @HiveField(0) required this.title,
+    @HiveField(1) required this.subTitle,
+    @HiveField(2) required this.date,
+    @HiveField(3) required this.color,
   });
 }
