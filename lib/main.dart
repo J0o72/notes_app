572bf4ab3b401e5ad8068cbc510d74345d6e2fa7
Youssef <youssefabdallah2016@gmail.com>
2024-09-16 04:50:56 +0300

    adding Font Family

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: "Poppins",
+      ),
       debugShowCheckedModeBanner: false,
       home: const HomeView(),
     );
