47a34fa43afbc692243b1a2aa70ef0a341320483
Youssef <youssefabdallah2016@gmail.com>
2024-09-17 03:10:53 +0300

    add BlocProvider

--- lib/Features/note/presentation/views/widgets/custom_modal_bottom_sheet.dart
+++ lib/Features/note/presentation/views/widgets/custom_modal_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/Features/note/presentation/manager/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/Features/note/presentation/views/widgets/custom_model_bottom_sheet_contents.dart';
 
 class CustomModalBottomSheet extends StatelessWidget {
@@ -6,10 +8,13 @@ class CustomModalBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
-      child: SingleChildScrollView(
-        child: ModalBottomSheetContents(),
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: SingleChildScrollView(
+          child: ModalBottomSheetContents(),
+        ),
       ),
     );
   }
