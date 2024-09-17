9fa521418c944b444385c046724e03806c5ee905
Youssef <youssefabdallah2016@gmail.com>
2024-09-18 01:30:23 +0300

    Create Add Deleted Note Fun in Cubit

--- lib/Features/note/presentation/manager/cubits/deleted_notes_cubit/deleted_notes_cubit.dart
+++ lib/Features/note/presentation/manager/cubits/deleted_notes_cubit/deleted_notes_cubit.dart
@@ -1,7 +1,26 @@
+import 'dart:ui';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/Features/note/presentation/manager/models/note_model.dart';
+import 'package:notes_app/constants.dart';
 
 part 'deleted_notes_state.dart';
 
 class DeletedNotesCubit extends Cubit<DeletedNotesState> {
   DeletedNotesCubit() : super(DeletedNotesInitial());
+
+  Color color = const Color(0xff77E4C8);
+
+  void addDeletedNote(NoteModel note) async {
+    note.color = color.value;
+    emit(DeletedNotesSuccess());
+    try {
+      var notesBox = Hive.box<NoteModel>(kDeletedNotesBox);
+      await notesBox.add(note);
+      emit(DeletedNotesSuccess());
+    } catch (e) {
+      emit(DeletedNotesFailure(errorMessage: e.toString()));
+    }
+  }
 }
