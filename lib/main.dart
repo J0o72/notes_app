acb42f08cd809b2cb6f4fb5c739342030a40c2a7
Youssef <youssefabdallah2016@gmail.com>
2024-09-17 03:18:45 +0300

    Create BLoc Observer Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Features/note/presentation/manager/models/note_model.dart';
 import 'package:notes_app/Features/note/presentation/views/notes_view.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/core/widgets/bloc_observer.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNotesBox);
 
